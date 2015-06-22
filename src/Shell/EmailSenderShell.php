54d4abd6cf3d502af4c92302b590401beaebd5ae
Willen Goulart <willen@gwd-web.com.br>
2015-06-22 16:21:05 -0300

    Domain + Template Suport

--- src/Shell/EmailSenderShell.php
+++ src/Shell/EmailSenderShell.php
@@ -16,22 +16,27 @@ class EmailSenderShell extends Shell
 	}
 	
 	
-    public function main() {
+    public function main($domain) {
     	$emails_to_send = $this->Emails->find('all')->where(['sent'=>false]);
     	foreach($emails_to_send as $item){
-    		$email = new Email($item->profile);
-    		$email->subject($item->subject);
-		if(!empty($item->view_vars))
-			$email->viewVars(unserialize($item->view_vars));
-		if(!empty($item->email_to))
-	    		$email->to(unserialize($item->email_to));
-		if(!empty($item->cc))
-	    		$email->cc(unserialize($item->cc));
-		if(!empty($item->bcc))
-	    		$email->bcc(unserialize($item->bcc));
-    		$email->send("");
-    		unset($email);
-    		$this->Emails->markAsSent($item);
+			$email = new Email($item->profile);
+			$email->domain($domain);
+			$email->subject($item->subject);
+			if(!empty($item->template) && !empty($item->layout))
+				$email->template($item->template, $item->layout);
+			if(!empty($item->view_vars))
+				$email->viewVars(unserialize($item->view_vars));
+			if(!empty($item->email_to))
+				$email->to(unserialize($item->email_to));
+			if(!empty($item->cc))
+				$email->cc(unserialize($item->cc));
+			if(!empty($item->bcc))
+				$email->bcc(unserialize($item->bcc));
+			$email->send("");
+			unset($email);
+			$this->Emails->markAsSent($item);
+    		
     	}
     }
 }
+
